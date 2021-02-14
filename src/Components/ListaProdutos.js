0bdfbe27f64f3728612eb0875556daf3e19a06fb
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-02-13 22:37:56 -0300

    funções de sorting criadas

--- src/Components/ListaProdutos.js
+++ src/Components/ListaProdutos.js
@@ -2,35 +2,44 @@ import React, { useState, useEffect } from "react";
 import Produtos from "./Produtos.js";
 
 const ListaProdutos = () => {
+  const [produto, setProduto] = useState([]);
 
-    const [produto, setProduto] = useState([]);
-    const [carregado, setCarregado] = useState(false);
-  
-    useEffect(() => {
-      fetch("./products.json")
-        .then((r) => r.json())
-        .then((json) => {
-          setProduto(json);
-          setCarregado(true);
-        });
-    }, []);
-  
-  console.log(produto)
-
-
+  useEffect(() => {
+    fetch("./products.json")
+      .then((r) => r.json())
+      .then((json) => {
+        setProduto(json);
+      });
+  }, []);
 
   function orgPreco(event) {
     event.preventDefault();
-    console.log(event.currentTarget)
+    setProduto(
+      produto.sort((a, b) =>
+        a.price < b.price ? -1 : a.price === b.price ? 0 : 1
+      )
+    );
+    console.log(produto);
   }
 
   function orgScore(event) {
     event.preventDefault();
+    setProduto(
+      produto.sort((a, b) =>
+        a.score < b.score ? -1 : a.score === b.score ? 0 : 1
+      )
+    );
+    console.log(produto);
   }
 
   function orgAZ(event) {
     event.preventDefault();
+    setProduto(
+      produto.sort((a, b) => (a.name < b.name ? -1 : a.name === b.name ? 0 : 1))
+    );
+    console.log(produto);
   }
+
   return (
     <div className="mainContent">
       <div className="headerLista">
