3470ddc1c6689c55a65540b82c4e139f4a79e6d5
ErickPrado101 <epg1.prado@gmail.com>
2024-01-24 17:02:52 -0300

    teste

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css"
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>Desenvolvido por <a href="https://github.com/bryanbruzinga">Bryan da Silva Bruzinga</a></p>
+      <p>Desenvolvido por <a href="https://github.com/ErickPrado101">Erick Prado</a></p>
     </footer>
   );
 };
